eecf9ed1dcb9de79440831a3db7c35d86fe9cde5
JiangJYzZZ <43143097+JiangJYzZZ@users.noreply.github.com>
2019-01-01 22:24:59 +0800

    Update Option.cs
    2017302580059 蒋加尧

--- Assets/Scripts/Option.cs
+++ Assets/Scripts/Option.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Option : MonoBehaviour {
-
     protected Vector2 startPosition;
     protected Vector2 startMousePosition;
 
@@ -17,42 +16,49 @@ public class Option : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
-
+        mouseIsIn = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-
+        if(mouseIsIn == true)
+        {
+            manager.GetComponent<VerticalBarManager>().MoveTarget(Input.GetAxis("Mouse ScrollWheel") * -1000);
+        }
     }
 
     //拖拽事件
     void DragBegin()
     {
-
+        startPosition = manager.transform.position;
+        startMousePosition = Input.mousePosition;
 
     }
     void Drag()
     {
+        Vector2 movePosition = (Vector2)Input.mousePosition - startMousePosition;
+        startMousePosition = Input.mousePosition;
 
+        manager.GetComponent<VerticalBarManager>().MoveTarget(movePosition.y);
     }
 
     //设置管理器
     public void SetManager(GameObject gameObject)
     {
-
+        manager = gameObject;
     }
 
     //鼠标是否在图标上
     void PointerIn()
     {
-  
+        mouseIsIn = true;
     }
     void PointerOut()
     {
-
+        mouseIsIn = false;
     }
     void Select()
     {
-     
+        manager.GetComponent<VerticalBarManager>().Select(gameObject);
     }
 }

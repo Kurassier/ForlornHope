6fc2a8510db0856270acea2b0469af947651955e
JiangJYzZZ <43143097+JiangJYzZZ@users.noreply.github.com>
2019-01-01 22:28:14 +0800

    Update GroupOption.cs
    2017302580059 蒋加尧

--- Assets/Scripts/GroupOption.cs
+++ Assets/Scripts/GroupOption.cs
@@ -1,8 +1,20 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GroupOption : Option{
- 
+    public Group template;
+
+    public void Initialize(Group group)
+    {
+        manager = GameObject.Find("SoldierManager");
+        template = group;
+        transform.Find("CardName").GetComponent<Text>().text = template.soldier.name + "(" + template.amount + ")";
+    }
+    public void ChangeAmount(int amount)
+    {
+        template.amount = amount;
+        transform.Find("CardName").GetComponent<Text>().text = template.soldier.name + "(" + template.amount + ")";
+    }
 }
